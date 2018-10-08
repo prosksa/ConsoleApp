0c89755b760837793993007d0d50221def6fce61
Сергей Проскурнин <prosksa88@gmail.com>
2018-10-08 19:31:49 +0300

    Добавьте файлы проекта.

---
+++ CSV_Parser/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Parser
+{
+	class Program
+	{
+		public static int[] maxSize = new int[31];
+		public static DateTime max, min;
+		public static DateTime[,] period = new DateTime[31, 2];
+
+		static void Main(string[] args)
+		{
+			string[] lines = File.ReadAllLines("task2.csv");
+			var sorted = lines.Select(line => new
+			{
+				SortKey = DateTime.Parse(line.Split(';')[1]),
+				Line = line
+			})
+				  .OrderBy(x => x.SortKey)
+				  .Select(x => x.Line);
+
+			Values[] values = sorted.Select(v => Values.FromCsv(v)).ToArray();
+
+			int c = 0;
+			int b = 1;
+			for (int a = 0; a < values.Length; a++)
+			{
+				if (b < values.Length)
+				{
+					max = values[a].end;
+					min = values[a].begin;
+					Loop(a, b, c, values);
+					b++;
+					c++;
+				}
+				period[a, 0] = min;
+				period[a, 1] = max;
+			}
+
+			int maxS = maxSize.Max();
+			int i = Array.IndexOf(maxSize, maxS);
+
+			Console.WriteLine($"В период с {period[i, 0]} по {period[i, 1]} был использован максимальный объем данных: {maxS}");
+			Console.ReadKey();
+		}
+
+		static void Loop(int a, int b, int c, Values[] values)
+		{
+			if (b < values.Length)
+			{
+
+				if (values[b].begin < values[a].end || values[b].begin < max)
+				{
+					if (values[a].end > max)
+					{
+						max = values[a].end;
+					}
+					maxSize[c] = maxSize[c] + values[a].size;
+					Loop(a + 1, b + 1, c, values);
+				}
+				else maxSize[c] = maxSize[c] + values[a].size;
+			}
+		}
+	}
+
+	class Values
+	{
+		public int id;
+		public DateTime begin;
+		public DateTime end;
+		public int size;
+
+		public static Values FromCsv(string csvLine)
+		{
+			string[] read = csvLine.Split(';');
+			Values values = new Values
+			{
+				id = int.Parse(read[0]),
+				begin = DateTime.Parse(read[1]),
+				end = DateTime.Parse(read[2]),
+				size = int.Parse(read[3])
+			};
+			return values;
+		}
+	}
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AVG_SD
+{
+	class Program
+	{
+		public static int min;
+		public static int max;
+		public static long ammount;
+		static List<long> list = new List<long>();
+		static Random random = new Random();
+
+		static void Main(string[] args)
+		{
+			#region Get info from User
+			Console.WriteLine("Please, enter minimum of number range (0-49999): ");
+			min = int.Parse(Console.ReadLine());
+			while (min < 0 || min >= 50000)
+			{
+				Console.WriteLine("Sorry, your number is out of range. Please, enter new number: ");
+				min = int.Parse(Console.ReadLine());
+			}
+
+			Console.WriteLine("Please, enter maximum of number range (1-50000): ");
+			max = int.Parse(Console.ReadLine());
+			while (max < 1 || max > 50000)
+			{
+				Console.WriteLine("Sorry, your number is out of range. Please, enter new number: ");
+				max = int.Parse(Console.ReadLine());
+			}
+
+			Console.WriteLine("Please, enter ammount of numbers to generate: ");
+			ammount = long.Parse(Console.ReadLine());
+			#endregion
+
+			DateTime start = DateTime.Now;
+
+			#region Generate Collection
+			Task task = Task.Factory.StartNew(TaskMethod);
+			Task.WaitAll(task);
+			#endregion
+
+			#region Avg and SD processing
+			Console.WriteLine($"Average value: {AverageValue(list)}");
+			Console.WriteLine($"Standard deviation: {StandardDeviation(list)}");
+			#endregion
+
+			TimeSpan timeSpent = DateTime.Now - start;
+			Console.WriteLine($"Memory used: {GC.GetTotalMemory(true)}");
+			Console.WriteLine($"Time spent: {timeSpent}");
+			Console.ReadLine();
+		}
+
+		private static void TaskMethod()
+		{
+			for (long a = 0; a < ammount; a++)
+			{
+				list.Add(random.Next(min, max));
+			}
+		}
+
+		private static long AverageValue(List<long> list)
+		{
+			return (long)list.Average();
+		}
+
+		private static long StandardDeviation(List<long> list)
+		{
+			long average = AverageValue(list);
+			long sum = 0;
+			foreach (long value in list)
+			{
+				sum += (value) * (value);
+			}
+			long sumAverage = sum / (list.Count - 1);
+			return (long)Math.Sqrt(sumAverage - (average * average));
+		}
+	}
+}
